ca18e4071cbf074025f95773272afaaf9d803444
arsenij-vdovichenko <vdovicenkoarsenij946@gmail.com>
2026-06-11 10:16:24 +0100

    Add CRUD functionality

--- src/app.js
+++ src/app.js
@@ -1,4 +1,65 @@
 
+const btnRef = document.getElementById("get-students-btn")
+const tbodyRef = document.querySelector("tbody")
+const formRef = document.getElementById("add-student-form")
+
+
+btnRef.addEventListener("click",()=>{
+getStudents().then((res)=>{
+    createStudents(res)
+})
+})
+
+
+
+function createStudents(array){
+    const item = array.map((students)=>{
+        return`<tr id="${students.id}">
+
+        <td>${students.id}</td>
+
+          <td>${students.name}</td>
+
+          <td>${students.age}</td>
+
+          <td>${students.course}</td>
+
+          <td>${students.skills}</td>
+
+          <td>${students.email}</td>
+
+          <td>${students.isEnrolled}</td>
+
+          <td>
+            <button type="button" data-action="edit">edit</button>
+            <button type="button" data-action="delete">delete</button>
+          </td>
+
+        </tr>
+`
+    }).join("")
+    tbodyRef.innerHTML = item
+    
+}
+
+formRef.addEventListener("submit", (event)=>{
+event.preventDefault()
+
+
+
+const data = {
+    name:event.currentTarget.elements[0].value,
+    age:event.currentTarget.elements[1].value,
+    course:event.currentTarget.elements[2].value,
+    skills:event.currentTarget.elements[3].value,
+    email:event.currentTarget.elements[4].value,
+    isEnrolled:event.currentTarget.elements[5].value
+
+
+}
+console.log(data);
+formRef.reset()
+})
 
 // Функція для отримання всіх студентів
 
@@ -6,10 +67,7 @@ function getStudents() {
 return fetch(`http://localhost:3000/students`).then((res)=>res.json())}
 
 
-getStudents().then((res)=>{
-    console.log(res);
-    
-})
+
 
 
 
