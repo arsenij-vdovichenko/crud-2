6792d05ec8fd05c0053367c774caf2cbb8bb3d4c
arsenij-vdovichenko <vdovicenkoarsenij946@gmail.com>
2026-06-14 11:08:16 +0100

    Add CRUD functionality

--- src/app.js
+++ src/app.js
@@ -1,20 +1,20 @@
+const btnRef = document.getElementById("get-students-btn");
+const tbodyRef = document.querySelector("tbody");
+const formRef = document.getElementById("add-student-form");
+let currentId = null;
 
-const btnRef = document.getElementById("get-students-btn")
-const tbodyRef = document.querySelector("tbody")
-const formRef = document.getElementById("add-student-form")
-let currentId = null
+btnRef.addEventListener("click", async () => {
+  const res = await getStudents();
+  await createStudents(res);
+  //   getStudents().then((res) => {
+  //     createStudents(res);
+  //   });
+});
 
-btnRef.addEventListener("click",()=>{
-getStudents().then((res)=>{
-    createStudents(res)
-})
-})
-
-
-
-function createStudents(array){
-    const item = array.map((students)=>{
-        return`<tr id="${students.id}">
+function createStudents(array) {
+  const item = array
+    .map((students) => {
+      return `<tr id="${students.id}">
 
         <td>${students.id}</td>
 
@@ -36,142 +36,138 @@ function createStudents(array){
           </td>
 
         </tr>
-`
-    }).join("")
-    tbodyRef.innerHTML = item
-    
-}
-
-formRef.addEventListener("submit", (event)=>{
-event.preventDefault()
-
-
-
-const data = {
-    name:event.currentTarget.elements[0].value,
-    age:event.currentTarget.elements[1].value,
-    course:event.currentTarget.elements[2].value,
-    skills:event.currentTarget.elements[3].value,
-    email:event.currentTarget.elements[4].value,
-    isEnrolled:event.currentTarget.elements[5].checked
-}
-if(currentId){
-    updateStudent(currentId, data).then(getStudents).then(res=>createStudents(res)).then(()=>{
-        currentId === null
-        formRef.reset()
+`;
     })
-    return
+    .join("");
+  tbodyRef.innerHTML = item;
 }
 
-addStudent(data).then(getStudents).then(res=>createStudents(res))
-formRef.reset()
-})
+formRef.addEventListener("submit", async (event) => {
+  event.preventDefault();
 
+  const data = {
+    name: event.currentTarget.elements[0].value,
+    age: event.currentTarget.elements[1].value,
+    course: event.currentTarget.elements[2].value,
+    skills: event.currentTarget.elements[3].value,
+    email: event.currentTarget.elements[4].value,
+    isEnrolled: event.currentTarget.elements[5].checked,
+  };
+  if (currentId) {
+    await updateStudent(currentId, data);
+    const res = await getStudents();
+    await createStudents(res);
+    currentId === null;
+    formRef.reset();
+
+    return;
+  }
+
+ await addStudent(data)
+ const res =await getStudents()
+ await createStudents(res)
+ formRef.reset();
+});
 
 // Функція для отримання всіх студентів
 
-function getStudents() {
-return fetch(`http://localhost:3000/students`).then((res)=>res.json())}
-
-
-
-
-
+async function getStudents() {
+  try {
+    //         const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    // const users = await response.json();
+    const res = await fetch("http://localhost:3000/students");
+    const users = await res.json();
+    return users;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
 
 // Функція для відображення студентів у таблиці
 
 function renderStudents(students) {
-
- // твій код
-
- 
-
+  // твій код
 }
-
-
 
 // Функція для додавання нового студента
 
-function addStudent(data) {
+async function addStudent(data) {
+  try {
     const options = {
+      method: "POST",
 
-method: "POST",
+      body: JSON.stringify(data),
 
-body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json; charset=UTF-8",
+      },
+    };
 
-headers: {
-
-"Content-Type": "application/json; charset=UTF-8",
-
-},
-
-};
-
-return fetch("http://localhost:3000/students", options)
-
+    return fetch("http://localhost:3000/students", options);
+  } catch (error) {
+    console.log(error);
+  }
 }
-
-
-
 
 // Функція для оновлення студента
 
-function updateStudent(id, data) {
-const options = {
+async function updateStudent(id, data) {
+  try {
+    const options = {
+      method: "PATCH",
 
-method: "PATCH",
+      body: JSON.stringify(data),
 
-body: JSON.stringify(data),
-
-headers: {
-
-"Content-Type": "application/json; charset=UTF-8",
-
-},
-
-};
-return fetch(`http://localhost:3000/students/${id}`, options)
-
- }
-
-
+      headers: {
+        "Content-Type": "application/json; charset=UTF-8",
+      },
+    };
+    return fetch(`http://localhost:3000/students/${id}`, options);
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 // Функція для видалення студента
 
-function deleteStudent(id) {
+async function deleteStudent(id) {
+  try {
     const options = {
-        method: "DELETE"
-    }
-    return fetch(`http://localhost:3000/students/${id}`, options)
-    
+      method: "DELETE",
+    };
+    return fetch(`http://localhost:3000/students/${id}`, options);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-tbodyRef.addEventListener("click",event=>{
+tbodyRef.addEventListener("click", async (event) => {
+  const action = event.target.dataset.action;
+  if (!action) {
+    return;
+  }
+  const tr = event.target.closest("tr");
+  const id = tr.id;
 
-    const action = event.target.dataset.action
-    if(!action){
-        return
-    }
-        const tr = event.target.closest("tr")
-        const id = tr.id
+  if (action === "delete") {
+    await deleteStudent(id);
+    const res = await getStudents();
+    await createStudents(res);
+    //   .then(getStudents)
+    //   .then((res) => createStudents(res));
+  }
 
-        if(action === "delete"){
-             deleteStudent(id).then(getStudents).then(res=>createStudents(res))
-        }
+  if (action === "edit") {
+    currentId = id;
+    console.log(currentId);
 
-        if(action === "edit"){
-            currentId = id
-            console.log(currentId);
-            
-            const td = tr.querySelectorAll("td");
-          document.getElementById("name").value = td[1].textContent;
-document.getElementById("age").value = td[2].textContent;
-document.getElementById("course").value = td[3].textContent;
-document.getElementById("skills").value = td[4].textContent;
-document.getElementById("email").value = td[5].textContent;
-document.getElementById("isEnrolled").checked =
-    td[6].textContent === "true";
-        }
-        
-        
-})
+    const td = tr.querySelectorAll("td");
+    document.getElementById("name").value = td[1].textContent;
+    document.getElementById("age").value = td[2].textContent;
+    document.getElementById("course").value = td[3].textContent;
+    document.getElementById("skills").value = td[4].textContent;
+    document.getElementById("email").value = td[5].textContent;
+    document.getElementById("isEnrolled").checked =
+      td[6].textContent === "true";
+  }
+});
